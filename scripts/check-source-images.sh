fe2c7a5f84d5ed5d5092ffdce09a1047f86b616b
ypjama <3822534+ypjama@users.noreply.github.com>
2021-04-18 17:47:42 +0300

    Check every subdirectory separately

--- scripts/check-source-images.sh
+++ scripts/check-source-images.sh
@@ -2,10 +2,18 @@
 # check-source-images.sh returns a non-zero status if
 # we don't have any source images.
 
-image_dir="assets/images/"
-num="$(find "${image_dir}" -type f -regextype posix-extended -regex '^.*\.[jpegpn]{3,}$'|wc -l)"
-if [ "$num" -lt 1 ]; then
-  echo >&2 "${image_dir} directory has zero valid images"
-  echo >&2 "add images before build"
-  exit 1
-fi
+project_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
+image_dir="${project_dir}/assets/images/"
+
+# Loop sub directories.
+while IFS= read -r -d '' dir
+do
+  # Check that this sub directory has atleast one valid image file.
+  num="$(find "${dir}" -type f -regextype posix-extended -regex '^.+\.(jpg|jpeg|png){1}$' | wc -l)"
+  if [ "$num" -lt 1 ]; then
+    echo >&2 "${dir} directory has zero valid images"
+    echo >&2 "add images before build"
+    exit 1
+  fi
+
+done < <(find "${image_dir}" -maxdepth 1 -mindepth 1 -type d -print0)
